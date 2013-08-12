e8dc279e6f823ee794bc23162b1465544f06cbf5
Jesica Niebuhr <jesi.niebuhr@fullsail.edu>
2013-08-12 03:20:24 -0400

    Added alert to file to ensure link between html and script.js files for AssociatingJS

--- Niebuhr_Jesica_AssociatingJS/JS/script.js
+++ Niebuhr_Jesica_AssociatingJS/JS/script.js
@@ -1,7 +1,1 @@
-/**
- * Created with JetBrains WebStorm.
- * User: jesiniebuhr
- * Date: 8/12/13
- * Time: 3:01 AM
- * To change this template use File | Settings | File Templates.
- */
+alert("Hello World!");
