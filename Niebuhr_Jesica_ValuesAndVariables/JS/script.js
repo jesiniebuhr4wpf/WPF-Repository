d2b525d2afa88b993c954cd5ef7daffff168a20e
Jesica Niebuhr <jesi.niebuhr@fullsail.edu>
2013-08-12 12:09:48 -0400

    Initial test to make sure files are associated.

--- Niebuhr_Jesica_ValuesAndVariables/JS/script.js
+++ Niebuhr_Jesica_ValuesAndVariables/JS/script.js
@@ -1,7 +1,2 @@
-/**
- * Created with JetBrains WebStorm.
- * User: jesiniebuhr
- * Date: 8/12/13
- * Time: 11:53 AM
- * To change this template use File | Settings | File Templates.
- */
+alert("Hello");
+
