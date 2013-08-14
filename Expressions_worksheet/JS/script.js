a8db26672c45d7630c1b7f71e5c46f18235b9016
Jesica Niebuhr <jesi.niebuhr@fullsail.edu>
2013-08-14 00:44:02 -0400

    set up initial variables for problem 2

--- Expressions_worksheet/JS/script.js
+++ Expressions_worksheet/JS/script.js
@@ -22,6 +22,36 @@ var dogYears = (age * 7);
 
 console.log("Sparky is" + " " + age + " " + "human years old which is" + " " + dogYears + " " + "in dog years.");
 
+//Slice of Pie Part 1
+    //A bunch of students are having a party and somebody ordered pizza. Create an
+        //expression that calculates how much pizza each partygoer will get at the
+        // party.  (Assume all pizzas have the same number of slices and that the
+        // person dividing the pizza is really precise, so this can be a decimal,
+        // like 3.52 slices, etc.)
+
+    //Given:
+        //Number of slices per pizza
+        //Number of people at the party
+        //Number of pizzas ordered.
+
+    //Result Variables:
+        //Number of slices per person. (Can be a decimal or floating point).
+
+    //Result to Print:
+        //“Each person ate X slices of pizza at the party.”
+
+//Calculate how much pizza each partygoer will get (assume all pizzas have same
+    //number of slices (the result can be a decimal).
+//possible expression for number of slices each person ate - sliPerPie * pizzas/people
+var sliPerPie = 8;
+var people = 13;
+var pizzas = 4;
+
+
+
+
+
+
 
 
 
