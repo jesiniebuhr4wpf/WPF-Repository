4bf97047c9a6ef87e9d83cb44f626f189652ead8
Jesica Niebuhr <jesi.niebuhr@fullsail.edu>
2013-08-14 00:25:55 -0400

    added example to work correct format started first problem initial expression to make sure expression is correct

--- Expressions_worksheet/JS/script.js
+++ Expressions_worksheet/JS/script.js
@@ -2,5 +2,29 @@
     //August 14, 2013
     //Activity: Expression Worksheet
 
-alert("test");
+//Example of format for problems
+    //Calculate the Circumference of a Circle
+    //var radius = 6
+    //const Pi = 3.14;
+
+    //multiply 2, Pi and the radius & assign to circ variable.
+    //var circ = radius * 2 * Pi;
+    //print out the output circ
+    //console.log("The circumference of the circle is " + circ);
+
+//Problem - Dog Years
+//Dogs age 7 times faster than humans so a dog that is 1 year old in
+    //human years is 7 years old in “dog years.”  Calculate how old Sparky the pit bull is in dog years based on his actual age.
+//Givens:
+    //Sparky’s age.
+//Result Varable:
+    //Sparky’s age in dog years.
+//Result to Print:
+    //“Sparky is X human years old which is X in dog years.
+
+//Problem Answer:
+
+var age = 7;
+var dogYears = (age * 2);
+console.log(dogYears);
 
