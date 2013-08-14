71a0b49b0e92728bb9fffafd16c7f3dab4c21e97
Jesica Niebuhr <jesi.niebuhr@fullsail.edu>
2013-08-13 23:26:33 -0400

    beginning understanding arithmetic operators

--- Screencast_Expressions/JS/script.js
+++ Screencast_Expressions/JS/script.js
@@ -6,3 +6,14 @@ b = a + 3;       //add 3 to a
 console.log(b);
 
 //include an assignment operator in order to store the result of the expression.
+
+
+//Arithmetic Operators
+
+//finding the are of a triangle which is half of width times height
+
+var width = 4;
+var height = 5;
+
+
+
