0977308d7e8fbbf632a28c770b4047bd91cfa7c8
Jesica Niebuhr <jesi.niebuhr@fullsail.edu>
2013-08-13 21:41:16 -0400

    understanding how the assignment operator works when changing values

--- Screencast_Expressions/JS/script.js
+++ Screencast_Expressions/JS/script.js
@@ -2,7 +2,7 @@
 
 
 var a = 2;       //sets up the variable and defines with value of 2
-a = a + 3;       //add 3 to a
-console.log(a);
+b = a + 3;       //add 3 to a
+console.log(b);
 
 //include an assignment operator in order to store the result of the expression.
