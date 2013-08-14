543afdcdd72e82d7dbc162269b4d64a1af75a223
Jesica Niebuhr <jesi.niebuhr@fullsail.edu>
2013-08-13 23:39:45 -0400

    added console to check the correct value of expression

--- Screencast_Expressions/JS/script.js
+++ Screencast_Expressions/JS/script.js
@@ -29,7 +29,8 @@ console.log(remainder);
 
 var orangeBins = [234, 567, 883];
 
-var total = orangeBins[0];      //don't forget to use array access notation
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];      //don't forget to use array access notation
+console.log(total);
 
 
 
