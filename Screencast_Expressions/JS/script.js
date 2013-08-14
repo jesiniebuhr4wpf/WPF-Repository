506177065c5eb3ca0ca6c520c30825343bfb1b60
Jesica Niebuhr <jesi.niebuhr@fullsail.edu>
2013-08-13 23:50:33 -0400

    added additional expression to understand when to and not to use parentheses

--- Screencast_Expressions/JS/script.js
+++ Screencast_Expressions/JS/script.js
@@ -38,6 +38,12 @@ var quiz4 = 80;
 var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;        //don't forget PEMDAS
 console.log(average);
 
+var length = 7;
+var width = 6
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
+
+
 
 //Using Arrays in Expressions
 
