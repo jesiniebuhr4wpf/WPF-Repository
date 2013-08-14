0393ee468f2ce0d1b181286862313d53615033cd
Jesica Niebuhr <jesi.niebuhr@fullsail.edu>
2013-08-13 21:40:07 -0400

    understanding how to store assignment operators for expressions

--- Screencast_Expressions/JS/script.js
+++ Screencast_Expressions/JS/script.js
@@ -1,1 +1,8 @@
-alert("test")
+//Expressions
+
+
+var a = 2;       //sets up the variable and defines with value of 2
+a = a + 3;       //add 3 to a
+console.log(a);
+
+//include an assignment operator in order to store the result of the expression.
